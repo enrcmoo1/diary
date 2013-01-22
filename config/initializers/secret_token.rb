1abfe6f8beebd9ffecc15ce51448724477d2f3ca
enrcmoo1 <C.Moore1@2010.ljmu.ac.uk>
2013-01-22 14:15:15 +0000

    Created new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Diary::Application.config.secret_token = 'b0137b017be60e102c6b3856fe7d1a72543cec984f3df2eaed2f110619c8253d76bb06c68128b74444d942c09d27cbc68ce2cdb964a50599b41f87b22291b052'
